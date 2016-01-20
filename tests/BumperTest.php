97a0ac2cf96044512153f151410ab9f9d03158f2
Igor T <igor.talevski@gmail.com>
2016-01-20 03:16:22 +0100

    add some testing setup

---
+++ tests/BumperTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class BumperTest extends PHPUnit_Framework_TestCase
+{
+	
+	function __construct()
+	{
+	}
+
+
+    /** @test */
+    public function it_will_increment_patch_version(){
+    	$this->assertFalse(false);
+    }
+}
